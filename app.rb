046f701d78dbfcd2627737001eada6e6c0721ca2
Ian Marshall <imarshall14@gmail.com>
2021-01-28 10:27:16 -0500

    commit tree dc0859e63491c66048c5d11e44ae97ebabc7fc8b

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        erb :index
+    end 
+
+    post '/checkout' do
+        @session = session
+        # item = params[:item]
+        #binding.pry
+        @session[:item]=params[:item]
+    end
+
 end
